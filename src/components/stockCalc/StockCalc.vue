4138dfe02b716b01baa796be644c76d3b39d720f
Lin xinliang <63276356+xllin0050@users.noreply.github.com>
2021-03-08 14:25:57 +0800

    easy view

--- src/components/stockCalc/StockCalc.vue
+++ src/components/stockCalc/StockCalc.vue
@@ -4,35 +4,47 @@
     <form class="buyStockForm">
       <!-- 輸入欄位 -->
       <input class="buyStockInput" placeholder="請輸入股號" type="text" name="stockSymbol" v-model="stockSymbol" @keypress="isShow=true">
+
       <input class="buyStockInput" placeholder="請輸入價位" type="text" name="strikePrice" v-model="strikePrice">
+
       <input class="buyStockInput" placeholder="請輸入張數" type="text" name="boardLot" v-model="boardLot">
+
       <!-- 輸入顯示 -->
       <div class="displayInput" v-show="isShow">
+
         <span >{{ stockSymbol }}</span>
+
         <span class="displayInput-style">價格</span>{{ strikePrice }}<span class="displayInput-style">元</span>
+
         <span >{{ boardLot }}</span><span class="displayInput-style">張</span>
       </div>
+
       <!-- 按鈕 -->
       <button class="buyStockBtn" type="button" @click="addStock(stockSymbol,strikePrice,boardLot)">送出</button>
     </form>
+
     <!-- 顯示紀錄區 -->
     <div class="stockHolding" v-for='(data, index) in inputCost' :key='index'>
       <div class="stockHolding-info">
-        <span class="stockHolding-info-style">股號</span><span>{{data.stockSymbol}}</span> 
-        <span class="stockHolding-info-style">買進價</span><span>{{data.strikePrice}}</span>
-        <span class="stockHolding-info-style">張數</span><span>{{data.boardLot}}</span>
+        <span class="stockHolding-info-style">股號</span>
+        <span>{{data.stockSymbol}}</span> 
+        <span class="stockHolding-info-style">買進價</span>
+        <span>{{data.strikePrice}}</span>
+        <span class="stockHolding-info-style">張數</span>
+        <span>{{data.boardLot}}</span>
       </div>
       <div class="stockHolding-cost">
-        <span class="stockHolding-cost-style">總價</span><span>{{data.strikeCost}}</span>
+        <span class="stockHolding-cost-style">總價</span>
+        <span>{{data.strikeCost}}</span>
         <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  
   name:'StockCalc',
   title:'Stock Trading Note',
   data(){
